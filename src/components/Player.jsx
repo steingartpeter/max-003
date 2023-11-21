9ba5bdf0a16e88970fd6fca8ee1811b7681edbe8
steingartpeter <steingart.p2@gmail.com>
2023-11-21 17:39:28 +0100

    UseState based on previous state

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -4,13 +4,18 @@ const Player = ({ name, symbol }) => {
   const [isEditing, setIsEditing] = useState(false);
 
   const handleEditClick = () => {
-    setIsEditing(true);
-    console.log("onEdit runs");
+    // setIsEditing(!isEditing) <= just scheduled, not executed
+    // this is not optimal, becausing of REACT SCHEDULing,
+    // it is not guaranteed, that no code will run which changes the value, we need
+    // the previous value, to which we have access in a parameter function
+    setIsEditing((editing) => !editing);
   };
 
+  let btnCaption = "Edit";
   let playerName = <span className="player-name">{name}</span>;
   if (isEditing) {
-    playerName = <input type="text" required />;
+    playerName = <input type="text" required value={name} />;
+    btnCaption = "Save";
   }
 
   return (
@@ -19,7 +24,7 @@ const Player = ({ name, symbol }) => {
         {playerName}
         <span className="player-symbol">{symbol}</span>
       </span>
-      <button onClick={handleEditClick}>Edit</button>
+      <button onClick={handleEditClick}>{isEditing ? "Save" : "Edit"}</button>
     </li>
   );
 };
